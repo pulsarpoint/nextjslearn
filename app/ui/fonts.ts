8cb966013d12e3edf8b16966f701a6cd8b733706
Goran Raovic <goran.raovic@gmail.com>
2024-10-04 00:45:01 +0200

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Lustria } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lustria({ weight: '400', subsets: ['latin'] });
